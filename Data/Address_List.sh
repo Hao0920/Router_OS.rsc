48f5b303b411f3870d8175ff8ec3809b0bb7412f
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-15 17:52:46 +0800

    Update Address_List.sh

--- Data/Address_List.sh
+++ Data/Address_List.sh
@@ -1,21 +1,22 @@
 mkdir -p ./ISP-IP
 cd ./ISP-IP
 
-#中国大陆所有IP地址
+# 获取 IP 列表
+# 中国大陆所有 IP 地址
 wget --no-check-certificate -c -O all_cn_cidr.txt https://ispip.clang.cn/all_cn_cidr.txt
-#中国电信
+# 中国电信
 wget --no-check-certificate -c -O chinatelecom_cidr.txt https://ispip.clang.cn/chinatelecom_cidr.txt
-#中国联通/网通
+# 中国联通/网通
 wget --no-check-certificate -c -O unicom_cnc_cidr.txt https://ispip.clang.cn/unicom_cnc_cidr.txt
-#中国移动
+# 中国移动
 wget --no-check-certificate -c -O cmcc_cidr.txt https://ispip.clang.cn/cmcc_cidr.txt
-#中国铁通
+# 中国铁通
 wget --no-check-certificate -c -O crtc_cidr.txt https://ispip.clang.cn/crtc_cidr.txt
-#中国教育网
+# 中国教育网
 wget --no-check-certificate -c -O cernet_cidr.txt https://ispip.clang.cn/cernet_cidr.txt
-#长城宽带/鹏博士
+# 长城宽带/鹏博士
 wget --no-check-certificate -c -O gwbn_cidr.txt https://ispip.clang.cn/gwbn_cidr.txt
-#中国其他ISP
+# 中国其他 ISP
 wget --no-check-certificate -c -O othernet_cidr.txt https://ispip.clang.cn/othernet_cidr.txt
 
 #构建 Address List
@@ -63,5 +64,5 @@ for net in $(cat othernet_cidr.txt) ; do
 done
 } > ../Address_List.rsc
 
-cd ..
-rm -rf ./ISP-IP
+#cd ..
+#rm -rf ./ISP-IP
