9330ff1a744357538e1c583c3a7ec17688cb6635
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-16 13:10:53 +0800

    Update DNS_Static.sh

--- DNS_Static/DNS_Static.sh
+++ DNS_Static/DNS_Static.sh
@@ -3,25 +3,18 @@ mkdir -p ./DNS_Static/Cache
 # 获取 DNS_Static 列表
 # 亚马逊 Alexa 助手
 wget --no-check-certificate -c -O ./DNS_Static/Cache/alexa.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/alexa
-sed -i -e '/#/d;/^$/d' ./DNS_Static/Cache/alexa.txt
 # 苹果
 wget --no-check-certificate -c -O ./DNS_Static/Cache/apple.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/apple
-sed -i -e '/#/d;/^$/d' ./DNS_Static/Cache/apple.txt
 # 华为
 wget --no-check-certificate -c -O ./DNS_Static/Cache/huawei.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/huawei
-sed -i -e '/#/d;/^$/d' ./DNS_Static/Cache/huawei.txt
 # Roku
 wget --no-check-certificate -c -O ./DNS_Static/Cache/roku.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/roku
-sed -i -e '/#/d;/^$/d' ./DNS_Static/Cache/roku.txt
 # 三星
 wget --no-check-certificate -c -O ./DNS_Static/Cache/samsung.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/samsung
-sed -i -e '/#/d;/^$/d' ./DNS_Static/Cache/samsung.txt
 # Sonos
 wget --no-check-certificate -c -O ./DNS_Static/Cache/sonos.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/sonos
-sed -i -e '/#/d;/^$/d' ./DNS_Static/Cache/sonos.txt
 # Windows
 wget --no-check-certificate -c -O ./DNS_Static/Cache/windows.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/windows
-sed -i -e '/#/d;/^$/d' ./DNS_Static/Cache/windows.txt
 # 小米
 wget --no-check-certificate -c -O ./DNS_Static/Cache/xiaomi.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/xiaomi
 
