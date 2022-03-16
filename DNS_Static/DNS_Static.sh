ca943b2621713dabf6f4de6c2473fa042c72a3b7
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-16 12:10:16 +0800

    Update DNS_Static.sh

--- DNS_Static/DNS_Static.sh
+++ DNS_Static/DNS_Static.sh
@@ -3,36 +3,36 @@ mkdir -p ./DNS_Static/Cache
 # 获取 DNS_Static 列表
 # 亚马逊 Alexa 助手
 wget --no-check-certificate -c -O ./DNS_Static/Cache/alexa.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/alexa
-grep -v "^#" ./DNS_Static/Cache/alexa.txt
-grep -v "^$" ./DNS_Static/Cache/alexa.txt
+sed -i -e '/#/d;/^$/d' ./DNS_Static/Cache/alexa.txt
+
 # 苹果
 wget --no-check-certificate -c -O ./DNS_Static/Cache/apple.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/apple
-grep -v "^#" ./DNS_Static/Cache/apple.txt
-grep -v "^$" ./DNS_Static/Cache/apple.txt
+sed -i -e '/#/d;/^$/d' ./DNS_Static/Cache/apple.txt
+
 # 华为
 wget --no-check-certificate -c -O ./DNS_Static/Cache/huawei.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/huawei
-grep -v "^#" ./DNS_Static/Cache/huawei.txt
-grep -v "^$" ./DNS_Static/Cache/huawei.txt
+sed -i -e '/#/d;/^$/d' ./DNS_Static/Cache/huawei.txt
+
 # Roku
 wget --no-check-certificate -c -O ./DNS_Static/Cache/roku.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/roku
-grep -v "^#" ./DNS_Static/Cache/roku.txt
-grep -v "^$" ./DNS_Static/Cache/roku.txt
+sed -i -e '/#/d;/^$/d' ./DNS_Static/Cache/roku.txt
+
 # 三星
 wget --no-check-certificate -c -O ./DNS_Static/Cache/samsung.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/samsung
-grep -v "^#" ./DNS_Static/Cache/samsung.txt
-grep -v "^$" ./DNS_Static/Cache/samsung.txt
+sed -i -e '/#/d;/^$/d' ./DNS_Static/Cache/samsung.txt
+
 # Sonos
 wget --no-check-certificate -c -O ./DNS_Static/Cache/sonos.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/sonos
-grep -v "^#" ./DNS_Static/Cache/sonos.txt
-grep -v "^$" ./DNS_Static/Cache/sonos.txt
+sed -i -e '/#/d;/^$/d' ./DNS_Static/Cache/sonos.txt
+
 # Windows
 wget --no-check-certificate -c -O ./DNS_Static/Cache/windows.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/windows
-grep -v "^#" ./DNS_Static/Cache/windows.txt
-grep -v "^$" ./DNS_Static/Cache/windows.txt
+sed -i -e '/#/d;/^$/d' ./DNS_Static/Cache/windows.txt
+
 # 小米
 wget --no-check-certificate -c -O ./DNS_Static/Cache/xiaomi.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/xiaomi
-grep -v "^#" ./DNS_Static/Cache/xiaomi.txt
-grep -v "^$" ./DNS_Static/Cache/xiaomi.txt
+sed -i -e '/#/d;/^$/d' ./DNS_Static/Cache/xiaomi.txt
+
 
 #构建 DNS Static
 {
@@ -40,6 +40,7 @@ echo "/ip dns static"
 echo "remove [find address="240.0.0.1"]"
 
 for name in $(cat ./DNS_Static/Cache/alexa.txt) ; do
+  
   echo "add address="240.0.0.1" name=$name"
 done
 
