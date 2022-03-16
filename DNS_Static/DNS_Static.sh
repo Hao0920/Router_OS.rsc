87bedec0e12a995db5c8e7f0130785263f64f0b3
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-16 10:31:00 +0800

    Update DNS_Static.sh

--- DNS_Static/DNS_Static.sh
+++ DNS_Static/DNS_Static.sh
@@ -3,20 +3,28 @@ mkdir -p ./DNS_Static/Cache
 # 获取 DNS_Static 列表
 # 亚马逊 Alexa 助手
 wget --no-check-certificate -c -O ./DNS_Static/Cache/alexa.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/alexa
+grep -v "^#" alexa.txt | grep -v "^$" >> alexa.txt
 # 苹果
 wget --no-check-certificate -c -O ./DNS_Static/Cache/apple.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/apple
+grep -v "^#" apple.txt | grep -v "^$" >> apple.txt
 # 华为
 wget --no-check-certificate -c -O ./DNS_Static/Cache/huawei.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/huawei
+grep -v "^#" huawei.txt | grep -v "^$" >> huawei.txt
 # Roku
 wget --no-check-certificate -c -O ./DNS_Static/Cache/roku.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/roku
+grep -v "^#" roku.txt | grep -v "^$" >> roku.txt
 # 三星
 wget --no-check-certificate -c -O ./DNS_Static/Cache/samsung.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/samsung
+grep -v "^#" samsung.txt | grep -v "^$" >> samsung.txt
 # Sonos
 wget --no-check-certificate -c -O ./DNS_Static/Cache/sonos.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/sonos
+grep -v "^#" sonos.txt | grep -v "^$" >> sonos.txt
 # Windows
 wget --no-check-certificate -c -O ./DNS_Static/Cache/windows.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/windows
+grep -v "^#" windows.txt | grep -v "^$" >> windows.txt
 # 小米
 wget --no-check-certificate -c -O ./DNS_Static/Cache/xiaomi.txt https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/xiaomi
+grep -v "^#" xiaomi.txt | grep -v "^$" >> xiaomi.txt
 
 #构建 DNS Static
 {
@@ -24,42 +32,34 @@ echo "/ip dns static"
 echo "remove [find address="240.0.0.1"]"
 
 for name in $(cat ./DNS_Static/Cache/alexa.txt) ; do
-  sed -i -e '/#/d;/^$/d'
   echo "add address="240.0.0.1" name=$name"
 done
 
 for name in $(cat ./DNS_Static/Cache/apple.txt) ; do
-  sed -i -e '/#/d;/^$/d'
   echo "add address="240.0.0.1" name=$name"
 done
 
 for name in $(cat ./DNS_Static/Cache/huawei.txt) ; do
-  sed -i -e '/#/d;/^$/d'
   echo "add address="240.0.0.1" name=$name"
 done
 
 for name in $(cat ./DNS_Static/Cache/roku.txt) ; do
-  sed -i -e '/#/d;/^$/d'
   echo "add address="240.0.0.1" name=$name"
 done
 
 for name in $(cat ./DNS_Static/Cache/samsung.txt) ; do
-  sed -i -e '/#/d;/^$/d'
   echo "add address="240.0.0.1" name=$name"
 done
 
 for name in $(cat ./DNS_Static/Cache/sonos.txt) ; do
-  sed -i -e '/#/d;/^$/d'
   echo "add address="240.0.0.1" name=$name"
 done
 
 for name in $(cat ./DNS_Static/Cache/windows.txt) ; do
-  sed -i -e '/#/d;/^$/d'
   echo "add address="240.0.0.1" name=$name"
 done
 
 for name in $(cat ./DNS_Static/Cache/xiaomi.txt) ; do
-  sed -i -e '/#/d;/^$/d'
   echo "add address="240.0.0.1" name=$name"
 done
 } > ./DNS_Static.rsc
