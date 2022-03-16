234807ec4c966edcd18eae2011922da86493791c
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-16 15:03:01 +0800

    Update DNS_Static.sh

--- DNS_Static/DNS_Static.sh
+++ DNS_Static/DNS_Static.sh
@@ -1,29 +1,49 @@
 mkdir -p ./DNS_Static/Cache
-cd ./DNS_Static/Cache
 # 获取 DNS_Static 列表
 # 亚马逊 Alexa 助手
-wget --no-check-certificate -c -O "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/alexa" \
-| sed -i -c -e '/^#/d' | sort | uniq >alexa.txt
+wget --no-check-certificate -c -O ./DNS_Static/Cache/alexa.txt "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/alexa"
 # 苹果
-wget --no-check-certificate -c -O "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/apple" \
-| sed -i -c -e '/^#/d' | sort | uniq >apple.txt
+wget --no-check-certificate -c -O ./DNS_Static/Cache/apple.txt "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/apple"
 # 华为
-wget --no-check-certificate -c -O "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/huawei" \
-| sed -i -c -e '/^#/d' | sort | uniq >huawei.txt
+wget --no-check-certificate -c -O ./DNS_Static/Cache/huawei.txt "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/huawei"
 # Roku
-wget --no-check-certificate -c -O "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/roku" \
-| sed -i -c -e '/^#/d' | sort | uniq >roku.txt
+wget --no-check-certificate -c -O ./DNS_Static/Cache/roku.txt "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/roku"
 # 三星
-wget --no-check-certificate -c -O "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/samsung" \
-| sed -i -c -e '/^#/d' | sort | uniq >samsung.txt
+wget --no-check-certificate -c -O ./DNS_Static/Cache/samsung.txt "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/samsung"
 # Sonos
-wget --no-check-certificate -c -O "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/sonos" \
-| sed -i -c -e '/^#/d' | sort | uniq >sonos.txt
+wget --no-check-certificate -c -O ./DNS_Static/Cache/sonos.txt "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/sonos"
 # Windows
-wget --no-check-certificate -c -O "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/windows" \
-| sed -i -c -e '/^#/d' | sort | uniq >windows.txt
+wget --no-check-certificate -c -O ./DNS_Static/Cache/windows.txt "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/windows"
 # 小米
-wget --no-check-certificate -c -O "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/xiaomi" \
-| sed -i -c -e '/^#/d' | sort | uniq >xiaomi.txt
+wget --no-check-certificate -c -O ./DNS_Static/Cache/xiaomi.txt "https://raw.githubusercontent.com/nextdns/metadata/master/privacy/native/xiaomi"
 
+#构建
+{
+  echo "/ip dns static"
+  echo "remove [find address=240.0.0.1]"
+  for name in $(cat ./DNS_Static/Cache/alexa.txt) ; do
+    sort | sort | grep -Ev '(#.*$)|(^$)' | echo "add address=240.0.0.1 name=$name"
+  done
+  for name in $(cat ./DNS_Static/Cache/apple.txt) ; do
+    sort | sort | grep -Ev '(#.*$)|(^$)' | echo "add address=240.0.0.1 name=$name"
+  done
+  for name in $(cat ./DNS_Static/Cache/huawei.txt) ; do
+    sort | sort | grep -Ev '(#.*$)|(^$)' | echo "add address=240.0.0.1 name=$name"
+  done
+  for name in $(cat ./DNS_Static/Cache/roku.txt) ; do
+    sort | sort | grep -Ev '(#.*$)|(^$)' | echo "add address=240.0.0.1 name=$name"
+  done
+  for name in $(cat ./DNS_Static/Cache/samsung.txt) ; do
+    sort | sort | grep -Ev '(#.*$)|(^$)' | echo "add address=240.0.0.1 name=$name"
+  done
+  for name in $(cat ./DNS_Static/Cache/sonos.txt) ; do
+    sort | sort | grep -Ev '(#.*$)|(^$)' | echo "add address=240.0.0.1 name=$name"
+  done
+  for name in $(cat ./DNS_Static/Cache/windows.txt) ; do
+    sort | sort | grep -Ev '(#.*$)|(^$)' | echo "add address=240.0.0.1 name=$name"
+  done
+  for name in $(cat ./DNS_Static/Cache/xiaomi.txt) ; do
+    sort | sort | grep -Ev '(#.*$)|(^$)' | echo "add address=240.0.0.1 name=$name"
+  done
 
+} > ./DNS_Static.rsc
