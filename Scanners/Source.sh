e0f5bde2d32b5d4a08f822d8c36b38a56ebd08de
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-21 01:14:34 +0800

    Update Source.sh

--- Scanners/Source.sh
+++ Scanners/Source.sh
@@ -1,7 +1,7 @@
 ### 建立缓存 ### 
 mkdir -p ./Scanners/Cache/Source/
 #### 获取 Scanners 列表 ###
-DATE="$(echo $(date +%Y%m%d%H%M%S))"
+DATE="$(echo $(date +"%Y-%m-%d %H:%M:%S"))"
 # 东北大学网络中心
 wget --no-check-certificate -c -O ./Scanners/Cache/Source/$DATE.txt "http://antivirus.neu.edu.cn/ssh/lists/neu.txt"
 # 清除缓存
