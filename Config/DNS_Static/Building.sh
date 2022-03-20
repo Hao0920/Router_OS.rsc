1167abcee2634aa7acd21242d62bea80ee04854b
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-20 23:03:15 +0800

    Update Building.sh

--- Config/DNS_Static/Building.sh
+++ Config/DNS_Static/Building.sh
@@ -1,6 +1,8 @@
 ### 建立缓存 ### 
 mkdir -p ./Config/DNS_Static/Cache/Building/
-#
+### 整理 ###
 find ./Config/DNS_Static/Cache/Source -type f -name "*.txt" | xargs cat > ./Config/DNS_Static/Cache/Building/1.txt
+# 删除注释行
 sed -e '/^#/d' ./Config/DNS_Static/Cache/Building/1.txt > ./Config/DNS_Static/Cache/Building/2.txt
-sed -e '/ @ads/d' ./Config/DNS_Static/Cache/Building/2.txt > ./Config/DNS_Static/Cache/Building/3.txt
+# 删除 @ads
+sed -e 's/ @ads//' ./Config/DNS_Static/Cache/Building/2.txt > ./Config/DNS_Static/Cache/Building/3.txt
