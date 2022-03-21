a33cb5da89bef4063bd622063d7facd7a06595e2
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-22 00:13:41 +0800

    Update Build.sh

--- Adblock/Build.sh
+++ Adblock/Build.sh
@@ -2,10 +2,10 @@
 mkdir -p ./Adblock/Cache/Build/
 
 ### 整理 ###
-find ./Adblock/Cache/Upstream -type f -name "*.txt" | xargs cat > ./Adblock/Cache/Build/upstream.txt
-find ./Adblock/Cache/Source -name "*.txt" | xargs cat > ./Adblock/Cache/Build/source.txt
+find ./Adblock/Cache/Upstream -type f -name "*.txt" | xargs cat > ./Adblock/Cache/Build/1.txt
+find ./Adblock/Source -type f -name "*.txt" | xargs cat > ./Adblock/Cache/Build/source.txt
 # 删除注释行
-sed -e '/^#/d' ./Adblock/Cache/Build/upstream.txt > ./Adblock/Cache/Build/2.txt
+sed -e '/^#/d' ./Adblock/Cache/Build/1.txt > ./Adblock/Cache/Build/2.txt
 # 删除 include 行
 sed -e '/^include:/d' ./Adblock/Cache/Build/2.txt > ./Adblock/Cache/Build/3.txt
 # 删除 regexp 行
