d9a3ffcee8859e9911036558be801f4f648ce23b
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-23 12:58:19 +0800

    Update

--- Scanners/Build.sh
+++ Scanners/Build.sh
@@ -2,22 +2,18 @@
 mkdir -p ./Scanners/Cache/Build/
 
 ### 整理 ###
-find ./Scanners/Cache/Source -type f -name "*.txt" | xargs cat > ./Scanners/Cache/Build/1.txt
-# 删除注释行
-sed -e '/^#/d' ./Scanners/Cache/Build/1.txt > ./Scanners/Cache/Build/2.txt
-# 删除空格行
-sed -e '/^$/d' ./Scanners/Cache/Build/2.txt > ./Scanners/Cache/Build/3.txt
-# 去重
-sort -u ./Scanners/Cache/Build/3.txt > ./Scanners/Cache/Build/4.txt
+sed -e '/:/d' ./Scanners/Cache/Upstream/ustc-blackip.txt > ./Scanners/Cache/Upstream/ustc-blackip-1.txt
+sed -e '/^#.*/d' ./Scanners/Cache/Upstream/ustc-blackip-1.txt > ./Scanners/Cache/Upstream/ustc-blackip-2.txt
 
 ### 构建 ###
 {
 echo "/ip firewall address-list"
 echo "remove [find list="scanners"]"
-for address in $(cat ./Scanners/Cache/Build/4.txt) ; do
+for address in $(cat ./Scanners/Cache/Upstream/ustc-blackip-2.txt) ; do
   echo "add list=scanners address=$address"
 done
+echo "/file remove scanners.rsc"
 } > ./Scanners/Scanners.rsc
 
 ### 清除缓存 ###
-rm -rf ./Scanners/Cache/Build/
+#rm -rf ./Scanners/Cache/Build/
--- Scanners/Source.sh
+++ Scanners/Source.sh
@@ -1,9 +1,11 @@
-### 建立缓存 ### 
-mkdir -p ./Scanners/Cache/Source/
+### 建立缓存 ###
+mkdir -p ./Scanners/Cache/Upstream/
+
 #### 获取 Scanners 列表 ###
-DATE="$(echo $(date +"%Y-%m-%d %H:%M:%S"))"
-# 东北大学网络中心
-wget --no-check-certificate -c -O ./Scanners/Cache/Source/$DATE.txt "http://antivirus.neu.edu.cn/ssh/lists/neu.txt"
+# 中国科学技术大学
+wget --no-check-certificate -c -O ./Scanners/Cache/Upstream/ustc-blackip.txt "https://blackip.ustc.edu.cn/list.php?txt"
+
 # 清除缓存
-Old=$(date -d -30day +"%Y%m%d").txt
-rm -f ./Scanners/Cache/Source/$Old
+rm -f ./Scanners/Cache/Upstream/
+
+
