23996b37c22296716eb1061c03034f7123b22e55
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-23 13:29:14 +0800

    Update Build.sh

--- Scanners/Build.sh
+++ Scanners/Build.sh
@@ -2,15 +2,15 @@
 mkdir -p ./Scanners/Cache/Build/
 
 ### 整理 ###
-find ./Scanners/Cache/Upstream/ -type f -name "*.txt" | xargs cat > ./Adblock/Cache/Build/Upstream-1.txt
-sed -e '/:/d' ./Adblock/Cache/Build/Upstream-1.txt > ./Adblock/Cache/Build/Upstream-2.txt
-sed -e '/^#.*/d' ./Adblock/Cache/Build/Upstream-2.txt > ./Adblock/Cache/Build/Upstream-3.txt
+find ./Scanners/Cache/Upstream/ -type f -name "*.txt" | xargs cat > ./Scanners/Cache/Build/Upstream-1.txt
+sed -e '/:/d' ./Scanners/Cache/Build/Upstream-1.txt > ./Scanners/Cache/Build/Upstream-2.txt
+sed -e '/^#.*/d' ./Scanners/Cache/Build/Upstream-2.txt > ./Scanners/Cache/Build/Upstream-3.txt
 
 ### 构建 ###
 {
 echo "/ip firewall address-list"
 echo "remove [find list="scanners"]"
-for address in $(cat ./Adblock/Cache/Build/Upstream-3.txt) ; do
+for address in $(cat ./Scanners/Cache/Build/Upstream-3.txt) ; do
   echo "add list=scanners address=$address"
 done
 echo "/file remove scanners.rsc"
