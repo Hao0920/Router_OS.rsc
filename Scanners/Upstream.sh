063ff3748da853f7c776676bace006c167c9b7d1
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-23 13:01:55 +0800

    Update Upstream.sh

--- Scanners/Upstream.sh
+++ Scanners/Upstream.sh
@@ -6,6 +6,6 @@ mkdir -p ./Scanners/Cache/Upstream/
 wget --no-check-certificate -c -O ./Scanners/Cache/Upstream/ustc-blackip.txt "https://blackip.ustc.edu.cn/list.php?txt"
 
 # 清除缓存
-rm -f ./Scanners/Cache/Upstream/
+# rm -f ./Scanners/Cache/Upstream/
 
 
