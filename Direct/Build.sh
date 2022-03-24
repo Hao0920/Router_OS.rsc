0d11f605e4a4cb33bc4d839ecfb43349d433b479
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-24 14:25:44 +0800

    Update Build.sh

--- Direct/Build.sh
+++ Direct/Build.sh
@@ -10,7 +10,7 @@ sed -e '/^include:/d' ./Direct/Cache/Build/2.txt > ./Direct/Cache/Build/3.txt
 # 删除 regexp 行
 sed -e '/^regexp:/d' ./Direct/Cache/Build/3.txt > ./Direct/Cache/Build/4.txt
 # 找出  @cn行
-sed -n' @cn/p' ./Direct/Cache/Build/4.txt > ./Direct/Cache/Build/5.txt
+sed -n ' @cn/p' ./Direct/Cache/Build/4.txt > ./Direct/Cache/Build/5.txt
 # 删除  @cn
 sed -e 's/ @cn//' ./Direct/Cache/Build/5.txt > ./Direct/Cache/Build/6.txt
 # 删除 full:
