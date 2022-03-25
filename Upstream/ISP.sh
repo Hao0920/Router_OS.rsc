eaf1f8bb24ea875b569b5cf7267eaece5d73b035
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-25 15:12:17 +0800

    Update

--- Upstream/ISP.sh
+++ Upstream/ISP.sh
@@ -1,18 +1,18 @@
 #### 获取 ISP 列表 ####
 ### Clang.CN ###
 # 中国大陆所有 IP 地址
-wget --no-check-certificate -c -O ./ISP/Cache/all_cn.txt https://ispip.clang.cn/all_cn_cidr.txt
+wget --no-check-certificate -c -O ./Cache/ISP/Upstream/all_cn.txt https://ispip.clang.cn/all_cn_cidr.txt
 # 中国电信
-wget --no-check-certificate -c -O ./ISP/Cache/chinatelecom.txt https://ispip.clang.cn/chinatelecom_cidr.txt
+wget --no-check-certificate -c -O ./Cache/ISP/Upstream/chinatelecom.txt https://ispip.clang.cn/chinatelecom_cidr.txt
 # 中国联通/网通
-wget --no-check-certificate -c -O ./ISP/Cache/unicom_cnc.txt https://ispip.clang.cn/unicom_cnc_cidr.txt
+wget --no-check-certificate -c -O ./Cache/ISP/Upstream/unicom_cnc.txt https://ispip.clang.cn/unicom_cnc_cidr.txt
 # 中国移动
-wget --no-check-certificate -c -O ./ISP/Cache/cmcc.txt https://ispip.clang.cn/cmcc_cidr.txt
+wget --no-check-certificate -c -O ./Cache/ISP/Upstream/cmcc.txt https://ispip.clang.cn/cmcc_cidr.txt
 # 中国铁通
-wget --no-check-certificate -c -O ./ISP/Cache/crtc.txt https://ispip.clang.cn/crtc_cidr.txt
+wget --no-check-certificate -c -O ./Cache/ISP/Upstream/crtc.txt https://ispip.clang.cn/crtc_cidr.txt
 # 中国教育网
-wget --no-check-certificate -c -O ./ISP/Cache/cernet.txt https://ispip.clang.cn/cernet_cidr.txt
+wget --no-check-certificate -c -O ./Cache/ISP/Upstream/cernet.txt https://ispip.clang.cn/cernet_cidr.txt
 # 长城宽带/鹏博士
-wget --no-check-certificate -c -O ./ISP/Cache/gwbn.txt https://ispip.clang.cn/gwbn_cidr.txt
+wget --no-check-certificate -c -O ./Cache/ISP/Upstream/gwbn.txt https://ispip.clang.cn/gwbn_cidr.txt
 # 中国其他 ISP
-wget --no-check-certificate -c -O ./ISP/Cache/othernet.txt https://ispip.clang.cn/othernet_cidr.txt
+wget --no-check-certificate -c -O ./Cache/ISP/Upstream/othernet.txt https://ispip.clang.cn/othernet_cidr.txt
--- Upstream/Scanners.sh
+++ Upstream/Scanners.sh
@@ -1,8 +1,8 @@
 #### 获取 Scanners 列表 ####
 ### 中国科学技术大学 ###
 # IP黑名单
-wget --no-check-certificate -c -O ./Scanners/Cache/Upstream/ustc-list.txt "https://blackip.ustc.edu.cn/list.php?txt"
+wget --no-check-certificate -c -O ./Cache/Scanners/Upstream/ustc-list.txt "https://blackip.ustc.edu.cn/list.php?txt"
 # DNS客户黑名单
-wget --no-check-certificate -c -O ./Scanners/Cache/Upstream/ustc-dnsblackip.txt "https://blackip.ustc.edu.cn/dnsblackip.php?txt"
+wget --no-check-certificate -c -O ./Cache/Scanners/Upstream/ustc-dnsblackip.txt "https://blackip.ustc.edu.cn/dnsblackip.php?txt"
 # Mail客户黑名单
-wget --no-check-certificate -c -O ./Scanners/Cache/Upstream/ustc-mailblackip.txt "https://blackip.ustc.edu.cn/mailblackip.php?txt"
+wget --no-check-certificate -c -O ./Cache/Scanners/Upstream/ustc-mailblackip.txt "https://blackip.ustc.edu.cn/mailblackip.php?txt"
