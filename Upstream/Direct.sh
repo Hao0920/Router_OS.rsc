51962f04913e70a56716cf8863432aa274f21a91
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-25 14:52:03 +0800

    Update

---
+++ Upstream/Direct.sh
@@ -0,0 +1,3 @@
+#### 获取 Direct 列表 ####
+### Loyalsoldier/domain-list-custom ###
+wget --no-check-certificate -c -O ./Direct/Cache/Upstream/Loyalsoldier-geolocation-cn.txt "https://raw.githubusercontent.com/Loyalsoldier/domain-list-custom/release/geolocation-cn.txt"
---
+++ Upstream/Scanners.sh
@@ -0,0 +1,8 @@
+#### 获取 Scanners 列表 ####
+### 中国科学技术大学 ###
+# IP黑名单
+wget --no-check-certificate -c -O ./Scanners/Cache/Upstream/ustc-list.txt "https://blackip.ustc.edu.cn/list.php?txt"
+# DNS客户黑名单
+wget --no-check-certificate -c -O ./Scanners/Cache/Upstream/ustc-dnsblackip.txt "https://blackip.ustc.edu.cn/dnsblackip.php?txt"
+# Mail客户黑名单
+wget --no-check-certificate -c -O ./Scanners/Cache/Upstream/ustc-mailblackip.txt "https://blackip.ustc.edu.cn/mailblackip.php?txt"
