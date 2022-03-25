7ff5c2eea68d55091f94f8f73ffac7e8e454b487
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-25 16:27:14 +0800

    Update Build.sh

--- Direct/Build.sh
+++ Direct/Build.sh
@@ -2,30 +2,30 @@
 mkdir -p ./Direct/Cache/Build/
 
 ### 整理 ###
-find ./Direct/Cache/Upstream -type f -name "*.txt" | xargs cat > ./Direct/Cache/Build/1.txt
+find ./Direct/Cache/Upstream -type f -name "*.txt" | xargs cat > ./Direct/Cache/Build/direct-1.txt
 # 删除注释行
-sed -e '/^#/d' ./Direct/Cache/Build/1.txt > ./Direct/Cache/Build/2.txt
+sed -e '/^#/d' ./Direct/Cache/Build/direct-1.txt > ./Direct/Cache/Build/direct-2.txt
 # 删除 include 行
-sed -e '/^include:/d' ./Direct/Cache/Build/2.txt > ./Direct/Cache/Build/3.txt
+sed -e '/^include:/d' ./Direct/Cache/Build/direct-2.txt > ./Direct/Cache/Build/direct-3.txt
 # 删除 regexp 行
-sed -e '/^regexp:/d' ./Direct/Cache/Build/3.txt > ./Direct/Cache/Build/4.txt
+sed -e '/^regexp:/d' ./Direct/Cache/Build/direct-3.txt > ./Direct/Cache/Build/direct-4.txt
 # 删除  @cn
-sed -e 's/ @cn//' ./Direct/Cache/Build/4.txt > ./Direct/Cache/Build/5.txt
+sed -e 's/ @cn//' ./Direct/Cache/Build/direct-4.txt > ./Direct/Cache/Build/direct-5.txt
 # 删除 full:
-sed -e 's/full://' ./Direct/Cache/Build/5.txt > ./Direct/Cache/Build/6.txt
+sed -e 's/full://' ./Direct/Cache/Build/direct-5.txt > ./Direct/Cache/Build/direct-6.txt
 # 删除 0.0.0.0
-sed -e 's/0.0.0.0 //' ./Direct/Cache/Build/6.txt > ./Direct/Cache/Build/7.txt
+sed -e 's/0.0.0.0 //' ./Direct/Cache/Build/direct-6.txt > ./Direct/Cache/Build/direct-7.txt
 # 删除空格行
-sed -e '/^$/d' ./Direct/Cache/Build/7.txt > ./Direct/Cache/Build/8.txt
+sed -e '/^$/d' ./Direct/Cache/Build/direct-7.txt > ./Direct/Cache/Build/direct-8.txt
 # 去重
-sort -u ./Direct/Cache/Build/8.txt > ./Direct/Cache/Build/direct.txt
+sort -u ./Direct/Cache/Build/direct-8.txt > ./Direct/Cache/Build/direct.txt
 
 ### 构建 ###
 {
-echo "/ip dns static"
-echo "remove [find address=240.0.0.1]"
-for name in $(cat ./Direct/Cache/Build/direct.txt) ; do
-  echo "add address=240.0.0.1 name=$name"
+echo "/ip firewall address-list"
+echo "remove [find list="direct"]"
+for address in $(cat ./Direct/Cache/Build/direct.txt) ; do
+  echo "add list=direct address=$address"
 done
 echo "/file remove Direct.rsc"
 } > ./Direct/Direct.rsc
