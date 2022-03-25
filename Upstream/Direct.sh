4755bf03edc5ec4f973b12290ef933678ca59000
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-25 15:24:57 +0800

    Update

--- Upstream/Direct.sh
+++
@@ -1,3 +0,0 @@
-#### 获取 Direct 列表 ####
-### Loyalsoldier/domain-list-custom ###
-wget --no-check-certificate -c -O ./Cache/Direct/Upstream/Loyalsoldier-geolocation-cn.txt "https://raw.githubusercontent.com/Loyalsoldier/domain-list-custom/release/geolocation-cn.txt"
