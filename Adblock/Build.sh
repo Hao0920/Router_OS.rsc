5cff8dc0754703e5ea17e4e36455af4f1a2b7c56
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-25 23:10:37 +0800

    Update Build.sh

--- Adblock/Build.sh
+++ Adblock/Build.sh
@@ -24,31 +24,31 @@ sed -e 's/0.0.0.0 //' ./Adblock/Cache/Build/adblock-8.txt > ./Adblock/Cache/Buil
 # 删除 127.0.0.1
 sed -e 's/127.0.0.1 //' ./Adblock/Cache/Build/adblock-9.txt > ./Adblock/Cache/Build/adblock-10.txt
 # 删除 非屏蔽
-sed -e 's/livew.l.qq.com//' ./Adblock/Cache/Build/adblock-10.txt > ./Adblock/Cache/Build/adblock-11.txt
-sed -e 's/t7z.cupid.iqiyi.com//' ./Adblock/Cache/Build/adblock-11.txt > ./Adblock/Cache/Build/adblock-12.txt
-sed -e 's/wxsnsdy.wxs.qq.com//' ./Adblock/Cache/Build/adblock-12.txt > ./Adblock/Cache/Build/adblock-13.txt
-sed -e 's/is.snssdk.com//' ./Adblock/Cache/Build/adblock-13.txt > ./Adblock/Cache/Build/adblock-14.txt
-sed -e 's/adsmind.gdtimg.com//' ./Adblock/Cache/Build/adblock-14.txt > ./Adblock/Cache/Build/adblock-15.txt
-sed -e 's/pv.sohu.com//' ./Adblock/Cache/Build/adblock-15.txt > ./Adblock/Cache/Build/adblock-16.txt
-sed -e 's/gw.365you.com//' ./Adblock/Cache/Build/adblock-16.txt > ./Adblock/Cache/Build/adblock-17.txt
-sed -e 's/x.adnet.qq.com//' ./Adblock/Cache/Build/adblock-17.txt > ./Adblock/Cache/Build/adblock-18.txt
-sed -e 's/mi.gdt.qq.com//' ./Adblock/Cache/Build/adblock-18.txt > ./Adblock/Cache/Build/adblock-19.txt
-sed -e 's/c.gdt.qq.com//' ./Adblock/Cache/Build/adblock-19.txt > ./Adblock/Cache/Build/adblock-20.txt
-sed -e 's/d.gdt.qq.com//' ./Adblock/Cache/Build/adblock-20.txt > ./Adblock/Cache/Build/adblock-21.txt
-sed -e 's/i.gdt.qq.com//' ./Adblock/Cache/Build/adblock-21.txt > ./Adblock/Cache/Build/adblock-22.txt
-sed -e 's/m.gdt.qq.com//' ./Adblock/Cache/Build/adblock-22.txt > ./Adblock/Cache/Build/adblock-23.txt
-sed -e 's/t.gdt.qq.com//' ./Adblock/Cache/Build/adblock-23.txt > ./Adblock/Cache/Build/adblock-24.txt
-sed -e 's/v.gdt.qq.com//' ./Adblock/Cache/Build/adblock-24.txt > ./Adblock/Cache/Build/adblock-25.txt
-sed -e 's/q.i.gdt.qq.com//' ./Adblock/Cache/Build/adblock-25.txt > ./Adblock/Cache/Build/adblock-26.txt
-sed -e 's/v2.gdt.qq.com//' ./Adblock/Cache/Build/adblock-26.txt > ./Adblock/Cache/Build/adblock-27.txt
-sed -e 's/ii.gdt.qq.com//' ./Adblock/Cache/Build/adblock-27.txt > ./Adblock/Cache/Build/adblock-28.txt
-sed -e 's/rm.gdt.qq.com//' ./Adblock/Cache/Build/adblock-28.txt > ./Adblock/Cache/Build/adblock-29.txt
-sed -e 's/nc.gdt.qq.com//' ./Adblock/Cache/Build/adblock-29.txt > ./Adblock/Cache/Build/adblock-30.txt
-sed -e 's/vr.gdt.qq.com//' ./Adblock/Cache/Build/adblock-30.txt > ./Adblock/Cache/Build/adblock-31.txt
-sed -e 's/rpt.gdt.qq.com//' ./Adblock/Cache/Build/adblock-31.txt > ./Adblock/Cache/Build/adblock-32.txt
-sed -e 's/win.gdt.qq.com//' ./Adblock/Cache/Build/adblock-32.txt > ./Adblock/Cache/Build/adblock-33.txt
-sed -e 's/ipv4.gdt.qq.com//' ./Adblock/Cache/Build/adblock-33.txt > ./Adblock/Cache/Build/adblock-34.txt
-sed -e 's/v6ii.gdt.qq.com//' ./Adblock/Cache/Build/adblock-34.txt > ./Adblock/Cache/Build/adblock-35.txt
+sed -e '/^livew.l.qq/d' ./Adblock/Cache/Build/adblock-10.txt > ./Adblock/Cache/Build/adblock-11.txt
+sed -e '/^t7z.cupid.iqiyi/d' ./Adblock/Cache/Build/adblock-11.txt > ./Adblock/Cache/Build/adblock-12.txt
+sed -e '/^wxsnsdy.wxs.qq/d' ./Adblock/Cache/Build/adblock-12.txt > ./Adblock/Cache/Build/adblock-13.txt
+sed -e '/^is.snssdk/d' ./Adblock/Cache/Build/adblock-13.txt > ./Adblock/Cache/Build/adblock-14.txt
+sed -e '/^adsmind.gdtimg/d' ./Adblock/Cache/Build/adblock-14.txt > ./Adblock/Cache/Build/adblock-15.txt
+sed -e '/^pv.sohu/d' ./Adblock/Cache/Build/adblock-15.txt > ./Adblock/Cache/Build/adblock-16.txt
+sed -e '/^gw.365you/d' ./Adblock/Cache/Build/adblock-16.txt > ./Adblock/Cache/Build/adblock-17.txt
+sed -e '/^x.adnet.qq/d' ./Adblock/Cache/Build/adblock-17.txt > ./Adblock/Cache/Build/adblock-18.txt
+sed -e '/^mi.gdt.qq/d' ./Adblock/Cache/Build/adblock-18.txt > ./Adblock/Cache/Build/adblock-19.txt
+sed -e '/^c.gdt.qq/d' ./Adblock/Cache/Build/adblock-19.txt > ./Adblock/Cache/Build/adblock-20.txt
+sed -e '/^d.gdt.qq/d' ./Adblock/Cache/Build/adblock-20.txt > ./Adblock/Cache/Build/adblock-21.txt
+sed -e '/^i.gdt.qq/d' ./Adblock/Cache/Build/adblock-21.txt > ./Adblock/Cache/Build/adblock-22.txt
+sed -e '/^m.gdt.qq/d' ./Adblock/Cache/Build/adblock-22.txt > ./Adblock/Cache/Build/adblock-23.txt
+sed -e '/^t.gdt.qq/d' ./Adblock/Cache/Build/adblock-23.txt > ./Adblock/Cache/Build/adblock-24.txt
+sed -e '/^v.gdt.qq/d' ./Adblock/Cache/Build/adblock-24.txt > ./Adblock/Cache/Build/adblock-25.txt
+sed -e '/^q.i.gdt.qq/d' ./Adblock/Cache/Build/adblock-25.txt > ./Adblock/Cache/Build/adblock-26.txt
+sed -e '/^v2.gdt.qq/d' ./Adblock/Cache/Build/adblock-26.txt > ./Adblock/Cache/Build/adblock-27.txt
+sed -e '/^ii.gdt.qq/d' ./Adblock/Cache/Build/adblock-27.txt > ./Adblock/Cache/Build/adblock-28.txt
+sed -e '/^rm.gdt.qq/d' ./Adblock/Cache/Build/adblock-28.txt > ./Adblock/Cache/Build/adblock-29.txt
+sed -e '/^nc.gdt.qq/d' ./Adblock/Cache/Build/adblock-29.txt > ./Adblock/Cache/Build/adblock-30.txt
+sed -e '/^vr.gdt.qq/d' ./Adblock/Cache/Build/adblock-30.txt > ./Adblock/Cache/Build/adblock-31.txt
+sed -e '/^rpt.gdt.qq/d' ./Adblock/Cache/Build/adblock-31.txt > ./Adblock/Cache/Build/adblock-32.txt
+sed -e '/^win.gdt.qq/d' ./Adblock/Cache/Build/adblock-32.txt > ./Adblock/Cache/Build/adblock-33.txt
+sed -e '/^ipv4.gdt.qq/d' ./Adblock/Cache/Build/adblock-33.txt > ./Adblock/Cache/Build/adblock-34.txt
+sed -e '/^v6ii.gdt.qq/d' ./Adblock/Cache/Build/adblock-34.txt > ./Adblock/Cache/Build/adblock-35.txt
 # 删除空格行
 sed -e '/^$/d' ./Adblock/Cache/Build/adblock-35.txt > ./Adblock/Cache/Build/adblock-36.txt
 # 去重
