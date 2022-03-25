e2a04fd7ddb5670ab0762bc16ed17d6d1c8e854a
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-25 19:27:00 +0800

    Update Build.sh

--- Direct/Build.sh
+++ Direct/Build.sh
@@ -20,13 +20,13 @@ sed -e 's/domain://' ./Direct/Cache/Build/direct-7.txt > ./Direct/Cache/Build/di
 # 删除空格行
 sed -e '/^$/d' ./Direct/Cache/Build/direct-8.txt > ./Direct/Cache/Build/direct-9.txt
 # 去重
-sort -u ./Direct/Cache/Build/direct-9.txt > ./Direct/Cache/Build/direct-10.txt
-sed -e 's/\\n/|/' ./Direct/Cache/Build/direct-10.txt > ./Direct/Cache/Build/direct.txt
+sort -u ./Direct/Cache/Build/direct-9.txt > ./Direct/Cache/Build/direct.txt
+
 ### 构建 ###
 {
 echo "/ip firewall layer7-protocol"
 echo "remove [find name="direct"]"
-for address in $(cat ./Direct/Cache/Build/direct.txt) ; do
+for address in $(cat ./Direct/Cache/Build/direct.txt | tr '\n' '|') ; do
   echo "add name=direct regexp=$address"
 done
 echo "/file remove Direct.rsc"
