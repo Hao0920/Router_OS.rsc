1f93209410fe33a719789f0eb3ed075a2192973f
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-26 12:29:56 +0800

    Update Build.sh

--- Direct/Workflows/Build.sh
+++ Direct/Workflows/Build.sh
@@ -1,1 +1,12 @@
+### 构建 ###
+{
+echo "/ip firewall address-list"
+echo "remove [find name="direct"]"
+for address in $(cat ./Direct/Cache/Sorting/direct.txt) ; do
+  echo "add list=direct address=$address"
+done
+echo "/file remove Direct.rsc"
+} > ./Direct/Direct.rsc
 
+### 清除缓存 ###
+# rm -rf ./Direct/Cache
