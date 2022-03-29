b87a403f8d36f75ebf629fc9636ea55d509e5f90
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-29 10:26:58 +0800

    Delete Scanners directory

--- Scanners/Workflows/Sorting.sh
+++
@@ -1,7 +0,0 @@
-### 建立缓存 ### 
-mkdir -p ./Scanners/Cache/Sorting/
-
-### 整理 ###
-find ./Scanners/Cache/Upstream/ -type f -name "*.txt" | xargs cat > ./Scanners/Cache/Sorting/1.txt
-sed -e '/:/d' ./Scanners/Cache/Sorting/1.txt > ./Scanners/Cache/Sorting/2.txt
-sed -e '/^#.*/d' ./Scanners/Cache/Sorting/2.txt > ./Scanners/Cache/Sorting/scanners.txt
