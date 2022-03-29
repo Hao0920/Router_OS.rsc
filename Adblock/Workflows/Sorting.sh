401c1b29eeae419d9d46a1082aef0e596b97da1f
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-29 13:30:34 +0800

    Update Sorting.sh

--- Adblock/Workflows/Sorting.sh
+++ Adblock/Workflows/Sorting.sh
@@ -3,37 +3,37 @@ mkdir -p ./Adblock/Cache/Sorting
 find ./Adblock/Cache/Upstream/ -type f -name "*.txt" | xargs cat > ./Adblock/Cache/Sorting/Upstream-source.txt
 
 #### 整理 ####
-sed s/^0.0.0.0 //g \
-| sed s/:@ads$//g \
-| sed s/^domain://g \
-| sed s/^full://g \
-| sed s/^127.0.0.1 //g \
-| sed s/livew.l.qq.com//g \ 
-| sed s/t7z.cupid.iqiyi.com//g \ 
-| sed s/wxsnsdy.wxs.qq.com//g \ 
-| sed s/is.snssdk.com//g \ 
-| sed s/adsmind.gdtimg.com//g \ 
-| sed s/pv.sohu.com//g \ 
-| sed s/gw.365you.com//g \ 
-| sed s/x.adnet.qq.com//g \ 
-| sed s/mi.gdt.qq.com//g \ 
-| sed s/c.gdt.qq.com//g \ 
-| sed s/d.gdt.qq.com//g \ 
-| sed s/i.gdt.qq.com//g \ 
-| sed s/m.gdt.qq.com//g \ 
-| sed s/t.gdt.qq.com//g \ 
-| sed s/v.gdt.qq.com//g \ 
-| sed s/q.i.gdt.qq.com//g \ 
-| sed s/v2.gdt.qq.com//g \ 
-| sed s/ii.gdt.qq.com//g \ 
-| sed s/rm.gdt.qq.com//g \ 
-| sed s/nc.gdt.qq.com//g \ 
-| sed s/vr.gdt.qq.com//g \ 
-| sed s/rpt.gdt.qq.com//g \ 
-| sed s/win.gdt.qq.com//g \ 
-| sed s/ipv4.gdt.qq.com//g \ 
-| sed s/v6ii.gdt.qq.com//g \ 
-| sed /^#/d \ 
-| sed -e /^regexp:/d \ 
-| sed -e /localhost$/d \ 
-| sed -e /^$/d ./Adblock/Cache/Sorting/Upstream-source.txt > ./Adblock/Cache/Sorting/Upstream-sorting.txt
+sed -e 's/0.0.0.0 //' \
+| sed -e 's/:@ads//' \
+| sed -e 's/domain://' \
+| sed -e 's/full://' \
+| sed -e 's/127.0.0.1 //' \
+| sed -e 's/livew.l.qq.com//' \
+| sed -e 's/t7z.cupid.iqiyi.com//' \
+| sed -e 's/wxsnsdy.wxs.qq.com//' \
+| sed -e 's/is.snssdk.com//' \
+| sed -e 's/adsmind.gdtimg.com//' \
+| sed -e 's/pv.sohu.com//' \
+| sed -e 's/gw.365you.com//' \
+| sed -e 's/x.adnet.qq.com//' \
+| sed -e 's/mi.gdt.qq.com//' \
+| sed -e 's/c.gdt.qq.com//' \
+| sed -e 's/d.gdt.qq.com//' \
+| sed -e 's/i.gdt.qq.com//' \
+| sed -e 's/m.gdt.qq.com//' \
+| sed -e 's/t.gdt.qq.com//' \
+| sed -e 's/v.gdt.qq.com//' \
+| sed -e 's/q.i.gdt.qq.com//' \
+| sed -e 's/v2.gdt.qq.com//' \
+| sed -e 's/ii.gdt.qq.com//' \
+| sed -e 's/rm.gdt.qq.com//' \
+| sed -e 's/nc.gdt.qq.com//' \
+| sed -e 's/vr.gdt.qq.com//' \
+| sed -e 's/rpt.gdt.qq.com//' \
+| sed -e 's/win.gdt.qq.com//' \
+| sed -e 's/ipv4.gdt.qq.com//' \
+| sed -e 's/v6ii.gdt.qq.com//' \
+| sed -e '/^#/d' \ 
+| sed -e '/^regexp:/d' \ 
+| sed -e '/localhost$/d' \ 
+| sed -e '/^$/d' ./Adblock/Cache/Sorting/Upstream-source.txt > ./Adblock/Cache/Sorting/Upstream-sorting.txt
