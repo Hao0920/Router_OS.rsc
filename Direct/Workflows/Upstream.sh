8508de013282daa2bc0c5b08d6f002c5929d2800
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-30 19:15:06 +0800

    Update Upstream.sh

--- Direct/Workflows/Upstream.sh
+++ Direct/Workflows/Upstream.sh
@@ -1,5 +1,9 @@
 #### 获取 Direct 列表 ###
 # felixonmars/dnsmasq-china-list
-mkdir -p ./Direct/Cache/Upstream/felixonmars
-wget --no-check-certificate -c -O ./Direct/Cache/Upstream/felixonmars/apple.china.txt "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf"
-wget --no-check-certificate -c -O ./Direct/Cache/Upstream/felixonmars/google.china.txt "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf"
+mkdir -p ./Direct/Cache/Upstream/felixonmars/dnsmasq-china-list/
+wget --no-check-certificate -c -O ./Direct/Cache/Upstream/felixonmars/dnsmasq-china-list/apple.china.txt "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf"
+wget --no-check-certificate -c -O ./Direct/Cache/Upstream/felixonmars/dnsmasq-china-list/google.china.txt "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf"
+# v2fly/domain-list-community
+mkdir -p ./Direct/Cache/Upstream/v2fly/domain-list-community
+wget --no-check-certificate -c -O  "https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/apple" \
+grep -F '@cn' > ./Direct/Cache/Upstream/v2fly/domain-list-community/apple.txt
