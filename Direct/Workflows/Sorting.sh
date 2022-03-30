dd90b305e2bfbea4e8e856f999ae9c4359690c15
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-03-30 16:36:46 +0800

    Update Sorting.sh

--- Direct/Workflows/Sorting.sh
+++ Direct/Workflows/Sorting.sh
@@ -3,5 +3,5 @@ mkdir -p ./Direct/Cache/Sorting/
 
 ### 整理 ###
 find ./Direct/Cache/Upstream/ -type f -name "*.txt" | xargs cat > ./Direct/Cache/Sorting/1.txt
-sed -e 's/server=///' ./Direct/Cache/Sorting/1.txt > ./Direct/Cache/Sorting/2.txt
-sed -e 's//114.114.114.114//' ./Direct/Cache/Sorting/2.txt > ./Direct/Cache/Sorting/Direct.txt
+sed -e 's/server=\///' ./Direct/Cache/Sorting/1.txt > ./Direct/Cache/Sorting/2.txt
+sed -e 's/\/114.114.114.114//' ./Direct/Cache/Sorting/2.txt > ./Direct/Cache/Sorting/Direct.txt
