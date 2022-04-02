4fc7fcf8e8d1e88c3d3e3f5281c3134d2ba8cd2e
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-04-02 14:44:01 +0800

    Update Process.sh

--- Packages/Direct/Process.sh
+++ Packages/Direct/Process.sh
@@ -15,8 +15,7 @@ find ./Cache/Direct/Upstream/v2fly/domain-list-community -type f -name "*.txt" |
 grep -F '@cn' ./Cache/Direct/Process/v2fly/1.txt > ./Cache/Direct/Process/v2fly/2.txt
 sed -e '/#/d' ./Cache/Direct/Process/v2fly/2.txt > ./Cache/Direct/Process/v2fly/3.txt
 sed -e 's/full://' ./Cache/Direct/Process/v2fly/3.txt > ./Cache/Direct/Process/v2fly/4.txt
-sed -e 's/ @cn//' ./Cache/Direct/Process/v2fly/4.txt > ./Cache/Direct/Process/v2fly/5.txt
-sed -e '/regexp:/d' ./Cache/Direct/Process/v2fly/5.txt > ./Cache/Direct/Process/v2fly/domain-list-community.txt
+sed -e 's/ @cn//' ./Cache/Direct/Process/v2fly/4.txt > ./Cache/Direct/Process/v2fly/domain-list-community.txt
 {
     cat ./Cache/Direct/Process/Basic/Basic.txt
     cat ./Cache/Direct/Process/felixonmars/dnsmasq-china-list.txt
