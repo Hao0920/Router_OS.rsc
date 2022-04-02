3dd593b230515f3813ae3155bb07b753468645fa
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-04-02 09:40:01 +0800

    Update Build.sh

--- Packages/Direct/Build.sh
+++ Packages/Direct/Build.sh
@@ -3,8 +3,8 @@ mkdir -p ./Releases/Direct
 {
 echo "/ip firewall address-list"
 echo "remove [find list="direct"]"
-for name in $(cat ./Cache/Direct/Process/Origin.txt) ; do
-  echo "add name=$name"  type=FWD forward-to= 114.114.114.114,114.114.115.115,119.29.29.29,119.28.28.28,223.5.5.5,223.6.6.6
+for address in $(cat ./Cache/Direct/Process/Origin.txt) ; do
+  echo "add list=direct address=$address"
 done
 echo "/file remove Direct.rsc"
 } > ./Releases/Direct/Direct.rsc
