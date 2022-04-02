b7db42ed6f61adb87fdfc6036dded1f8f116139c
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-04-02 13:49:01 +0800

    Update Build.sh

--- Packages/Adblock/Build.sh
+++ Packages/Adblock/Build.sh
@@ -2,9 +2,9 @@
 mkdir -p ./Releases/Adblock
 {
 echo "/ip dns static"
-echo "remove [find type=NXDOMAIN]"
-for regexp in $(cat ./Cache/Adblock/Process/Basic/regexp.txt) ; do
-  echo "add type=NXDOMAIN regexp=\"$regexp\""
+echo "remove [find address=240.0.0.1]"
+for name in $(cat ./Cache/Adblock/Process/Origin.txt) ; do
+  echo "add address=240.0.0.1 name=$name"
 done
 echo "/file remove Adblock.rsc"
 } > ./Releases/Adblock/Adblock.rsc
