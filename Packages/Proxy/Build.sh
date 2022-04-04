28a82333dbef1e9f0e01a6570aada98b4b2f9d08
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-04-04 09:03:38 +0800

    Update

--- Packages/Proxy/Build.sh
+++ Packages/Proxy/Build.sh
@@ -3,7 +3,7 @@ mkdir -p ./Releases/Proxy
 {
 echo "/ip firewall address-list"
 echo "remove [find list="proxy"]"
-for address in $(cat ./Cache/Proxy/Upstream/Loyalsoldier/geoip/cn.txt) ; do
+for address in $(cat ./Cache/Proxy/Process/Loyalsoldier/geoip.txt) ; do
   echo "add list=proxy address=$address"
 done
 echo "/file remove Proxy.rsc"
--- Packages/Proxy/Process.sh
+++ Packages/Proxy/Process.sh
@@ -5,6 +5,6 @@ find ./Cache/Proxy/Upstream/Loyalsoldier/geoip -type f -name "*.txt" | xargs cat
 # 删除 IPv6
 sed -e '/:/d' ./Cache/Proxy/Process/Loyalsoldier/1.txt > ./Cache/Proxy/Process/Loyalsoldier/2.txt
 # 删除空格行
-sed -e '/^$/d' ./Cache/Proxy/Process/Loyalsoldier/3.txt > ./Cache/Proxy/Process/Loyalsoldier/4.txt
+sed -e '/^$/d' ./Cache/Proxy/Process/Loyalsoldier/2.txt > ./Cache/Proxy/Process/Loyalsoldier/3.txt
 # 去重
-sort -u ./Cache/Proxy/Process/Loyalsoldier/5.txt > ./Cache/Proxy/Process/Loyalsoldier/geoip.txt
+sort -u ./Cache/Proxy/Process/Loyalsoldier/3.txt > ./Cache/Proxy/Process/Loyalsoldier/geoip.txt
