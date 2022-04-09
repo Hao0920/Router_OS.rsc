e122fe042919037657059cef7dae608706ddc9b7
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-04-09 13:50:07 +0800

    Update

--- Packages/Adblock/Build.sh
+++ Packages/Adblock/Build.sh
@@ -4,11 +4,11 @@ mkdir -p ./Releases/Adblock
 echo "/ip dns static"
 echo "remove [find address=240.0.0.1]"
 for regexp in $(cat ./Cache/Adblock/Process/Basic/ACL4SSR/Regexp.txt) ; do
-  echo " add address=240.0.0.1 regexp="$regexp" "
+  echo "add address=240.0.0.1 regexp=$regexp"
 done
 echo "/ip firewall address-list"
 echo "remove [find list="adblock"]"
-for address in $(cat ./Cache/Adblock/Process/Basic/ACL4SSR/Regexp.txt) ; do
+for address in $(cat ./Cache/Adblock/Process/Basic/ACL4SSR/IPv4.txt) ; do
   echo "add list=adblock address=$address"
 done
 echo "/file remove Adblock.rsc"
