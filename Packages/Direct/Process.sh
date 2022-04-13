6c964d67cbb76a6f6e1e538febdcf694cbca728c
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-04-13 23:25:37 +0800

    Update Process.sh

--- Packages/Direct/Process.sh
+++ Packages/Direct/Process.sh
@@ -1,6 +1,11 @@
 #### 处理数据 ####
 ### Loyalsoldier/v2ray-rules-dat
-mkdir -p ./Cache/Direct/Process/Loyalsoldier
-find ./Cache/Direct/Upstream/Loyalsoldier/ -type f -name "*.txt" | xargs cat > ./Cache/Direct/Process/Loyalsoldier/1.txt
+mkdir -p ./Cache/Direct/Process/Loyalsoldier/v2ray-rules-dat
+find ./Cache/Direct/Upstream/Loyalsoldier/v2ray-rules-dat -type f -name "*.txt" | xargs cat > ./Cache/Direct/Process/Loyalsoldier/v2ray-rules-dat/1.txt
 # 删除 full:
-sed -e 's/full://' ./Cache/Direct/Process/Loyalsoldier/1.txt > ./Cache/Direct/Process/Loyalsoldier/v2ray-rules-dat.txt
+sed -e 's/full://' ./Cache/Direct/Process/Loyalsoldier/v2ray-rules-dat/1.txt > ./Cache/Direct/Process/Loyalsoldier/v2ray-rules-dat.txt
+
+### v2fly/domain-list-community
+mkdir -p ./Cache/Direct/Process/v2fly/domain-list-community
+find ./Cache/Direct/Upstream/v2fly/domain-list-community -type f -name "*.txt" | xargs cat > ./Cache/Direct/Process/v2fly/domain-list-community/1.txt
+grep "@cn" ./Cache/Direct/Process/v2fly/domain-list-community/1.txt > ./Cache/Direct/Process/v2fly/domain-list-community/2.txt
