206697371af6aa5313930a20724c989d57031e43
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-04-13 23:06:20 +0800

    Update Upstream.sh

--- Packages/Direct/Upstream.sh
+++ Packages/Direct/Upstream.sh
@@ -1,4 +1,5 @@
 #### 获取上游数据 ####
 ### Loyalsoldier/v2ray-rules-dat
 mkdir -p ./Cache/Direct/Upstream/Loyalsoldier/v2ray-rules-dat
-wget --no-check-certificate -c -O ./Cache/Direct/Upstream/Loyalsoldier/v2ray-rules-dat/direct-list.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/Loyalsoldier/v2ray-rules-dat/apple-cn.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/apple-cn.txt
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/Loyalsoldier/v2ray-rules-dat/google-cn.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/google-cn.txt
