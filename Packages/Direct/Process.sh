d4628641b65f3effb12138798fb40642a593f7ad
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-04-14 00:38:38 +0800

    Update Process.sh

--- Packages/Direct/Process.sh
+++ Packages/Direct/Process.sh
@@ -7,14 +7,14 @@ sed -e 's/full://' ./Cache/Direct/Process/Loyalsoldier/v2ray-rules-dat/1.txt > .
 
 ### v2fly/domain-list-community
 mkdir -p ./Cache/Direct/Process/v2fly/domain-list-community
-find ./Cache/Direct/Upstream/v2fly/domain-list-community -type f -name "*.txt" | xargs cat > ./Cache/Direct/Process/v2fly/domain-list-community.txt
-grep "@cn" ./Cache/Direct/Process/v2fly/domain-list-community.txt > ./Cache/Direct/Process/v2fly/cn.txt
+find ./Cache/Direct/Upstream/v2fly/domain-list-community -type f -name "*.txt" | xargs cat > ./Cache/Direct/Process/v2fly/domain-list-community/domain-list-community-1.txt
+grep "@cn" ./Cache/Direct/Process/v2fly/domain-list-community/domain-list-community-1.txt > ./Cache/Direct/Process/v2fly/domain-list-community/domain-list-community-cn.txt
 # domain
-sed -e 's/ @cn//'./Cache/Direct/Process/v2fly/cn.txt > ./Cache/Direct/Process/v2fly/domain-1.txt
-sed -e 's/full://' ./Cache/Direct/Process/v2fly/domain-1.txt > ./Cache/Direct/Process/v2fly/domain-2.txt
-sed -e '/#/d' ./Cache/Direct/Process/v2fly/domain-2.txt > ./Cache/Direct/Process/v2fly/domain-3.txt
-sed -e '/regexp:/d' ./Cache/Direct/Process/v2fly/domain-3.txt > ./Cache/Direct/Process/v2fly/domain-4.txt
-sort -u ./Cache/Direct/Process/v2fly/domain-4.txt > ./Cache/Direct/Process/v2fly/domain.txt
+sed -e 's/ @cn//'./Cache/Direct/Process/v2fly/domain-list-community/domain-list-community-cn.txt > ./Cache/Direct/Process/v2fly/domain-list-community/domain-1.txt
+sed -e 's/full://' ./Cache/Direct/Process/v2fly/domain-list-community/domain-1.txt > ./Cache/Direct/Process/v2fly/domain-list-community/domain-2.txt
+sed -e '/#/d' ./Cache/Direct/Process/v2fly/domain-list-community/domain-2.txt > ./Cache/Direct/Process/v2fly/domain-list-community/domain-3.txt
+sed -e '/regexp:/d' ./Cache/Direct/Process/v2fly/domain-list-community/domain-3.txt > ./Cache/Direct/Process/v2fly/domain-list-community/domain-4.txt
+sort -u ./Cache/Direct/Process/v2fly/domain-list-community/domain-4.txt > ./Cache/Direct/Process/v2fly/domain.txt
 # regexp
 grep "regexp:" ./Cache/Direct/Process/v2fly/cn.txt > ./Cache/Direct/Process/v2fly/regexp-1.txt
 sed -e 's/ @cn//' ./Cache/Direct/Process/v2fly/regexp-1.txt > ./Cache/Direct/Process/v2fly/regexp-2.txt
