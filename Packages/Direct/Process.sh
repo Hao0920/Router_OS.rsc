68a7773e481e895eba87f4a3e7f74b16b8123a03
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-04-13 23:06:27 +0800

    Update Process.sh

--- Packages/Direct/Process.sh
+++ Packages/Direct/Process.sh
@@ -3,7 +3,4 @@
 mkdir -p ./Cache/Direct/Process/Loyalsoldier
 find ./Cache/Direct/Upstream/Loyalsoldier/ -type f -name "*.txt" | xargs cat > ./Cache/Direct/Process/Loyalsoldier/1.txt
 # 删除 full:
-sed -e 's/full://' ./Cache/Direct/Process/Loyalsoldier/1.txt > ./Cache/Direct/Process/Loyalsoldier/2.txt
-# 删除regexp
-sed -e '/regexp:/d' ./Cache/Direct/Process/Loyalsoldier/2.txt > ./Cache/Direct/Process/Loyalsoldier/3.txt
-sort -u ./Cache/Direct/Process/Loyalsoldier/3.txt > ./Cache/Direct/Process/Loyalsoldier/v2ray-rules-dat.txt
+sed -e 's/full://' ./Cache/Direct/Process/Loyalsoldier/1.txt > ./Cache/Direct/Process/Loyalsoldier/v2ray-rules-dat.txt
