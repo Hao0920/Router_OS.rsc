cca28d4f69bff9532eca848663d6c474d9ed0354
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-04-14 01:28:52 +0800

    Update Build.sh

--- Packages/Direct/Build.sh
+++ Packages/Direct/Build.sh
@@ -3,7 +3,7 @@ mkdir -p ./Releases/Direct
 {
 echo "/ip dns static"
 echo "remove [find forward-to=10.0.0.1]"
-for name in $(cat ./Cache/Direct/Process/Loyalsoldier/v2ray-rules-dat.txt) ; do
+for name in $(cat ./Cache/Direct/Process/domain.txt) ; do
   echo "add forward-to=10.0.0.1 name=$name type=FWD"
 done
 echo "/file remove Direct.rsc"
