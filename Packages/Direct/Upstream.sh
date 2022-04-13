51bf6cde496a38ded2cd7c9f624ab61c308436c3
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-04-13 23:25:40 +0800

    Update Upstream.sh

--- Packages/Direct/Upstream.sh
+++ Packages/Direct/Upstream.sh
@@ -3,3 +3,26 @@
 mkdir -p ./Cache/Direct/Upstream/Loyalsoldier/v2ray-rules-dat
 wget --no-check-certificate -c -O ./Cache/Direct/Upstream/Loyalsoldier/v2ray-rules-dat/apple-cn.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/apple-cn.txt
 wget --no-check-certificate -c -O ./Cache/Direct/Upstream/Loyalsoldier/v2ray-rules-dat/google-cn.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/google-cn.txt
+
+### v2fly/domain-list-community
+mkdir -p ./Cache/Direct/Upstream/v2fly/domain-list-community
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/category-games.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/category-games
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/2kgames.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/2kgames
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/blizzard.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/blizzard
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/curseforge.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/curseforge
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/cygames.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/cygames
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/ea.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/ea
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/epicgames.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/epicgames
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/garena.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/garena
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/gog.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/gog
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/mihoyo.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/mihoyo
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/nintendo.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/nintendo
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/op.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/op
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/playstation.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/playstation
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/purikonejp.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/purikonejp
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/riot.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/riot
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/rockstar.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/rockstar
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/steam.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/steam
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/steamunlocked.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/steamunlocked
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/ubisoft.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/ubisoft
+wget --no-check-certificate -c -O ./Cache/Direct/Upstream/v2fly/domain-list-community/xbox.txt https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/xbox
