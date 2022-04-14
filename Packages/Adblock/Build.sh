6b269b72b42413814580f044ac2bde41369ff5fe
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-04-14 13:29:29 +0800

    Update Build.sh

--- Packages/Adblock/Build.sh
+++ Packages/Adblock/Build.sh
@@ -1,11 +1,11 @@
 ### 构建 Adblock.rsc ###
 mkdir -p ./Releases/Adblock
 {
-echo "/ip dns cache flush"
 echo "/ip dns static"
 echo "remove [find type=NXDOMAIN]"
 for name in $(cat ./Cache/Adblock/Process/Origin.txt) ; do
   echo "add name=$name type=NXDOMAIN"
 done
 echo "/file remove Adblock.rsc"
+echo "/ip dns cache flush"
 } > ./Releases/Adblock/Adblock.rsc
