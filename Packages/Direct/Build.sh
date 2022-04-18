63e70e571599d1c5b7c3bd179be754adc25625a4
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-04-18 23:26:38 +0800

    Update Build.sh

--- Packages/Direct/Build.sh
+++ Packages/Direct/Build.sh
@@ -1,10 +1,10 @@
 ### 构建 Direct.rsc ###
 mkdir -p ./Releases/Direct
 {
-echo "/ip firewall address-list"
-echo "remove [find list="direct"]"
-for address in $(cat ./Cache/Direct/Process/Process.txt) ; do
-  echo "add list=direct address=$address"
+echo "/ip dns static"
+echo "remove [find type=FWD]"
+for name in $(cat ./Cache/Direct/Process/Process.txt) ; do
+  echo "add forward-to=10.0.0.1 name=$name type=FWD"
 done
 echo "/file remove direct.rsc"
 } > ./Releases/Direct/Direct.rsc
