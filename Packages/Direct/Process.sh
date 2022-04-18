823dd3ce95cbbb7b7313b3d0abbd423a7b2e104b
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-04-18 15:29:05 +0800

    Update Process.sh

--- Packages/Direct/Process.sh
+++ Packages/Direct/Process.sh
@@ -1,4 +1,6 @@
 #### 处理数据 ####
 ### Basic
 mkdir -p ./Cache/Direct/Process/Basic
-find ./Packages/Direct/Basic -type f -name "*.txt" | xargs cat > ./Cache/Direct/Process/Basic.txt
+find ./Packages/Direct/Basic -type f -name "*.txt" | xargs cat > ./Cache/Direct/Process/Basic-1.txt
+sed -e '/^$/d' ./Cache/Direct/Process/Basic-1.txt > ./Cache/Direct/Process/Basic-2.txt
+sed -e '/#/d' ./Cache/Direct/Process/Basic-2.txt > ./Cache/Direct/Process/Basic.txt
