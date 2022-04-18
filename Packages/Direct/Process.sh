36263d6fbe6dcecc40280fd9c4b120e457879d56
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-04-18 22:46:31 +0800

    Update

--- Packages/Direct/Process.sh
+++ Packages/Direct/Process.sh
@@ -4,3 +4,19 @@ mkdir -p ./Cache/Direct/Process/Basic
 find ./Packages/Direct/Basic -type f -name "*.txt" | xargs cat > ./Cache/Direct/Process/Basic/Basic-1.txt
 sed -e '/^$/d' ./Cache/Direct/Process/Basic/Basic-1.txt > ./Cache/Direct/Process/Basic/Basic-2.txt
 sed -e '/#/d' ./Cache/Direct/Process/Basic/Basic-2.txt > ./Cache/Direct/Process/Basic/Basic.txt
+
+### v2fly/domain-list-community
+mkdir -p ./Cache/Direct/Process/v2fly/domain-list-community
+find ./Cache/Direct/Upstream/v2fly/domain-list-community -type f -name "*.txt" | xargs cat > ./Cache/Direct/Process/v2fly/domain-list-community/domain-list-community-1.txt
+grep "@cn" ./Cache/Direct/Process/v2fly/domain-list-community/domain-list-community-1.txt > ./Cache/Direct/Process/v2fly/domain-list-community/domain-list-community-2.txt
+sed -e 's/ @cn//' ./Cache/Direct/Process/v2fly/domain-list-community/domain-list-community-2.txt > ./Cache/Direct/Process/v2fly/domain-list-community/domain-list-community-3.txt
+sed -e 's/full://' ./Cache/Direct/Process/v2fly/domain-list-community/domain-list-community-3.txt > ./Cache/Direct/Process/v2fly/domain-list-community/domain-list-community-4.txt
+sed -e '/#/d' ./Cache/Direct/Process/v2fly/domain-list-community/domain-list-community-4.txt > ./Cache/Direct/Process/v2fly/domain-list-community/domain-list-community-5.txt
+sed -e '/regexp:/d' ./Cache/Direct/Process/v2fly/domain-list-community/domain-list-community-5.txt > ./Cache/Direct/Process/v2fly/domain-list-community/domain-list-community-6.txt
+
+{
+    cat ./Cache/Direct/Process/Basic/Basic.txt
+    cat ./Cache/Direct/Process/v2fly/domain-list-community/domain-list-community-6.txt
+    cat ./Cache/Direct/Upstream/v2fly/domain-list-community/category-pt.txt
+} > ./Cache/Direct/Process/Process-1.txt
+sort -u ./Cache/Direct/Process/Process-1.txt > ./Cache/Direct/Process/Process.txt
