516cafbc61c27314bc569cf38f4cd914a04fae9b
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-04-26 18:51:16 +0800

    Update Process.sh

--- Packages/Proxy/Process.sh
+++ Packages/Proxy/Process.sh
@@ -3,4 +3,5 @@
 mkdir -p ./Cache/Proxy/Process/Loyalsoldier/v2ray-rules-dat
 find ./Cache/Proxy/Upstream/Loyalsoldier/v2ray-rules-dat -type f -name "*.txt" | xargs cat > ./Cache/Proxy/Process/Loyalsoldier/v2ray-rules-dat/proxy-list-1.txt
 sed -e '/regexp:/d' ./Cache/Proxy/Process/Loyalsoldier/v2ray-rules-dat/proxy-list-1.txt > ./Cache/Proxy/Process/Loyalsoldier/v2ray-rules-dat/proxy-list-2.txt
-sed -e 's/full://' ./Cache/Proxy/Process/Loyalsoldier/v2ray-rules-dat/proxy-list-2.txt > ./Cache/Proxy/Process/Loyalsoldier/v2ray-rules-dat/proxy-list.txt
+sed -e 's/full://' ./Cache/Proxy/Process/Loyalsoldier/v2ray-rules-dat/proxy-list-2.txt > ./Cache/Proxy/Process/Loyalsoldier/v2ray-rules-dat/proxy-list-3.txt
+sort -u ./Cache/Proxy/Process/Loyalsoldier/v2ray-rules-dat/proxy-list-3.txt > ./Cache/Proxy/Process/Loyalsoldier/v2ray-rules-dat/proxy-list.txt
