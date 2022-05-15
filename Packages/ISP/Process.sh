07e10abd2e99d207845d7a45fa6c5c6a25a0398a
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-05-15 16:51:34 +0800

    Update

--- Packages/ISP/Process.sh
+++ Packages/ISP/Process.sh
@@ -7,3 +7,15 @@ sed -e '/:/d' ./Cache/ISP/Process/Loyalsoldier/1.txt > ./Cache/ISP/Process/Loyal
 # 删除空格行
 sed -e '/^$/d' ./Cache/ISP/Process/Loyalsoldier/2.txt > ./Cache/ISP/Process/Loyalsoldier/geoip.txt
 
+### Basic/geoip
+mkdir -p ./Cache/ISP/Process/Basic
+find ./Packages/ISP/Basic -type f -name "*.txt" | xargs cat > ./Cache/ISP/Process/Basic/1.txt
+# 删除 #
+sed -e '/#/d' ./Cache/ISP/Process/Basic/1.txt > ./Cache/ISP/Process/Basic/geoip.txt
+
+{
+    cat ./Cache/ISP/Process/Basic/geoip.txt
+    cat ./Cache/ISP/Process/Loyalsoldier/geoip.txt
+} > ./Cache/ISP/Process/Process-1.txt
+sort -u ./Cache/ISP/Process/Process-1.txt > ./Cache/ISP/Process/Process.txt
+
