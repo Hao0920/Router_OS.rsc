956f46da7bf8815edd24ac3546d7f4a848598d45
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-05-16 21:12:37 +0800

    Update Build.sh

--- Packages/Direct/Build.sh
+++ Packages/Direct/Build.sh
@@ -3,7 +3,7 @@ mkdir -p ./Releases/Direct
 {
 echo "/ip firewall address-list"
 echo "remove [find list="direct"]"
-for address in $(cat ./Cache/Direct/Process/Basic/Basic.txt) ; do
+for address in $(cat ./Cache/Direct/Process/Process.txt) ; do
   echo "add list=direct address=$address"
 done
 echo "/file remove Direct.rsc"
