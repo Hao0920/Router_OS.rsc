a298289d46e242215fbdd31318c5e144f25a2b12
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-07-09 12:25:09 +0800

    Update Process.sh

--- Packages/ISP/Process.sh
+++ Packages/ISP/Process.sh
@@ -1,8 +1,14 @@
 #### 处理数据 ####
 ### Loyalsoldier/geoip
 mkdir -p ./Cache/ISP/Process/Loyalsoldier
-find ./Cache/ISP/Upstream/Loyalsoldier/geoip -type f -name "*.txt" | xargs cat > ./Cache/ISP/Process/Loyalsoldier/1.txt
+find ./Cache/ISP/Upstream/Loyalsoldier/geoip -type f -name "*.txt" | xargs cat > ./Cache/ISP/Process/Loyalsoldier/ipv4-1.txt
 # 删除 IPv6
-sed -e '/:/d' ./Cache/ISP/Process/Loyalsoldier/1.txt > ./Cache/ISP/Process/Loyalsoldier/2.txt
+sed -e '/:/d' ./Cache/ISP/Process/Loyalsoldier/ipv4-1.txt > ./Cache/ISP/Process/Loyalsoldier/ipv4-2.txt
 # 删除空格行
-sed -e '/^$/d' ./Cache/ISP/Process/Loyalsoldier/2.txt > ./Cache/ISP/Process/Loyalsoldier/geoip.txt
+sed -e '/^$/d' ./Cache/ISP/Process/Loyalsoldier/ipv4-2.txt > ./Cache/ISP/Process/Loyalsoldier/geoip-ipv4.txt
+
+find ./Cache/ISP/Upstream/Loyalsoldier/geoip -type f -name "*.txt" | xargs cat > ./Cache/ISP/Process/Loyalsoldier/ipv6-1.txt
+# 删除 IPv4
+sed -e '/^(?:(?:\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.){3}(?:\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])$/d' ./Cache/ISP/Process/Loyalsoldier/ipv6-1.txt > ./Cache/ISP/Process/Loyalsoldier/ipv6-2.txt
+# 删除空格行
+sed -e '/^$/d' ./Cache/ISP/Process/Loyalsoldier/ipv6-2.txt > ./Cache/ISP/Process/Loyalsoldier/geoip-ipv6.txt
