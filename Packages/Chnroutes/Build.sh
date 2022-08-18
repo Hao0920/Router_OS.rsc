5496e9b7eeb45ab9fbe0fc5bccd9f689521ff8e3
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-08-18 22:46:47 +0800

    Create Build.sh

---
+++ Packages/Chnroutes/Build.sh
@@ -0,0 +1,30 @@
+### 构建 Chnroutes.rsc ###
+mkdir -p ./Releases/Chnroutes
+{
+    echo "/ip firewall address-list"
+    echo "remove [/ip firewall address-list find list="china"]"
+    for address in $(cat ./Cache/Chnroutes/Process/IPv4.txt) ; do
+    echo "add list=china address=$address"
+    done
+    echo "/file remove Chnroutes-IPv4.rsc"
+} > ./Releases/Chnroutes/Chnroutes-IPv4.rsc
+
+{
+    echo "/ipv6 firewall address-list"
+    echo "remove [/ipv6 firewall address-list find list="china"]"
+    for address in $(cat ./Cache/Chnroutes/Upstream/pexcn/daily/chnroute-v6.txt) ; do
+    echo "add list=china address=$address"
+    done
+    echo "/file remove Chnroutes-IPv6.rsc"
+} > ./Releases/Chnroutes/Chnroutes-IPv6.rsc
+
+{
+    echo "/ip dns static remove numbers=[/ip dns static find type=FWD]"
+    echo "/ip dns"
+    echo ":global gfwdns"
+    echo "/ip dns static"
+    for regexp in $(cat ./Cache/Chnroutes/Process/Loyalsoldier/v2ray-rules-dat/gfw.txt) ; do
+    echo "add regexp=$regexp type=FWD forward-to=$gfwdns"
+    echo "/ip dns cache flush"
+    echo "/file remove GFWFWD.rsc"
+} > ./Releases/Chnroutes/GFWFWD.rsc
