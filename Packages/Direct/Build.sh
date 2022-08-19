2e0a56850f87f357c38f7ffd4149d2a35148eaf0
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-08-19 13:48:38 +0800

    Delete

--- Packages/Direct/Build.sh
+++
@@ -1,9 +0,0 @@
-### 构建 Direct.rsc ###
-mkdir -p ./Releases/Direct
-{
-echo "/ip firewall address-list"
-for address in $(cat ./Cache/Direct/Process/Process.txt) ; do
-  echo "add list=china address=$address"
-done
-echo "/file remove Direct.rsc"
-} > ./Releases/Direct/Direct.rsc
