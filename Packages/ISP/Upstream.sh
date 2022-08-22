ac377ad72793c59f63bf5415b1abdfdbe4239ee9
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-08-22 23:45:35 +0800

    Update Upstream.sh

--- Packages/ISP/Upstream.sh
+++ Packages/ISP/Upstream.sh
@@ -1,8 +1,6 @@
 #### 获取上游数据 ####
 ### Clang.CN
 mkdir -p ./Cache/ISP/Upstream/Clang.CN
-# 中国大陆所有 IP 地址
-wget --no-check-certificate -c -O ./Cache/ISP/Upstream/Clang.CN/all_cn.txt https://ispip.clang.cn/all_cn_cidr.txt
 # 中国电信
 wget --no-check-certificate -c -O ./Cache/ISP/Upstream/Clang.CN/chinatelecom.txt https://ispip.clang.cn/chinatelecom_cidr.txt
 # 中国联通/网通
@@ -22,7 +20,3 @@ wget --no-check-certificate -c -O ./Cache/ISP/Upstream/Clang.CN/othernet.txt htt
 mkdir -p ./Cache/ISP/Upstream/Loyalsoldier/geoip
 wget --no-check-certificate -c -O ./Cache/ISP/Upstream/Loyalsoldier/geoip/cn.txt https://raw.githubusercontent.com/Loyalsoldier/geoip/release/text/cn.txt
 wget --no-check-certificate -c -O ./Cache/ISP/Upstream/Loyalsoldier/geoip/private.txt https://raw.githubusercontent.com/Loyalsoldier/geoip/release/text/private.txt
-
-### Hackl0us/GeoIP2-CN
-mkdir -p ./Cache/ISP/Upstream/Hackl0us/GeoIP2-CN
-wget --no-check-certificate -c -O ./Cache/ISP/Upstream/Hackl0us/GeoIP2-CN/CN-ip-cidr.txt https://raw.githubusercontent.com/Hackl0us/GeoIP2-CN/release/CN-ip-cidr.txt
