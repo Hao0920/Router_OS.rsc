991d7be4aa62338836f6f067693cc5e6f8548922
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-09-23 16:06:17 +0800

    Update Process.sh

--- Packages/Scanners/Process.sh
+++ Packages/Scanners/Process.sh
@@ -1,6 +1,7 @@
 #### 处理数据 ####
 # 中国科学技术大学
 mkdir -p ./Cache/Scanners/Process/USTC
-find ./Cache/Scanners/Upstream/USTC -type f -name "*.txt" | xargs cat > ./Cache/Scanners/Process/USTC/1.txt
-sed -e '/:/d' ./Cache/Scanners/Process/USTC/1.txt > ./Cache/Scanners/Process/USTC/2.txt
-sed -e '/^#.*/d' ./Cache/Scanners/Process/USTC/2.txt > ./Cache/Scanners/Process/USTC/scanners.txt
+find ./Cache/Scanners/Upstream/USTC -type f -name "*.txt" | xargs cat > ./Cache/Scanners/Process/USTC/Scanners.txt
+sed -e '/:/d' ./Cache/Scanners/Process/USTC/Scanners.txt > ./Cache/Scanners/Process/USTC/Scanners-IPv4-1.txt
+sed -e '/^#.*/d' ./Cache/Scanners/Process/USTC/Scanners-IPv4-1.txt > ./Cache/Scanners/Process/USTC/Scanners-IPv4-2.txt
+sort -u ./Cache/Scanners/Process/USTC/Scanners-IPv4-2.txt > ./Cache/Scanners/Process/USTC/Scanners-IPv4.txt
