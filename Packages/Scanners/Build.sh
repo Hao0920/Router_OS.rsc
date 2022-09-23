782186b1bb5b48e8d9a4310077ca2d09230fcbe6
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-09-23 16:06:14 +0800

    Update Build.sh

--- Packages/Scanners/Build.sh
+++ Packages/Scanners/Build.sh
@@ -1,10 +1,18 @@
 ### 构建 Scanners.rsc ###
 mkdir -p ./Releases/Scanners
 {
-echo "/ip firewall address-list"
-echo "remove [find list="scanners"]"
-for address in $(cat ./Cache/Scanners/Process/USTC/scanners.txt) ; do
+  echo "/ip firewall address-list"
+  echo "remove [find list="scanners"]"
+  for address in $(cat ./Cache/Scanners/Process/USTC/Scanners-IPv4.txt) ; do
   echo "add list=scanners address=$address"
-done
-echo "/file remove Scanners.rsc"
-} > ./Releases/Scanners/Scanners.rsc
+  done
+  echo "/file remove Scanners-IPv4.rsc"
+} > ./Releases/Scanners/Scanners-IPv4.rsc
+{
+  echo "/ipv6 firewall address-list"
+  echo "remove [find list="scanners"]"
+  for address in $(cat ./Cache/Scanners/Process/USTC/Scanners-IPv6.txt) ; do
+  echo "add list=scanners address=$address"
+  done
+  echo "/file remove Scanners-IPv6.rsc"
+} > ./Releases/Scanners/Scanners-IPv6.rsc
