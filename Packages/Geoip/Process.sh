12d50efc0f268a43e698408ff151622479a214d9
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-10-23 18:27:08 +0800

    Update Process.sh

--- Packages/Geoip/Process.sh
+++ Packages/Geoip/Process.sh
@@ -1,16 +1,20 @@
 #### 处理数据 ####
-## misakaio/chnroutes2
+# misakaio/chnroutes2
 mkdir -p ./Cache/Geoip/Process/misakaio/chnroutes2
 sed -e '/#/d' ./Cache/Geoip/Upstream/misakaio/chnroutes2/Geoip.txt > ./Cache/Geoip/Process/misakaio/chnroutes2/Geoip.txt
 
-## Loyalsoldier/geoip
+# Loyalsoldier/geoip
 mkdir -p ./Cache/Geoip/Process/Loyalsoldier/geoip
-# 删除 IPv6
+## 删除 IPv6
 sed -e '/:/d' ./Cache/Geoip/Upstream/Loyalsoldier/geoip/private.txt > ./Cache/Geoip/Process/Loyalsoldier/geoip/private-ipv4.txt
-# 删除 IPv4
+## 删除 IPv4
 sed -e '/\./d' ./Cache/Geoip/Upstream/Loyalsoldier/geoip/private.txt > ./Cache/Geoip/Process/Loyalsoldier/geoip/private-ipv6.txt
+
+# Source
+find ./Source/Geoip/IPv4 -type f -name "*.txt" | xargs cat > ./Cache/Geoip/Process/Source/IPv4.txt
+find ./Source/Geoip/IPv6 -type f -name "*.txt" | xargs cat > ./Cache/Geoip/Process/Source/IPv6.txt
 {
-    cat ./Source/Geoip/Apple_Push_Notification_Service/IPv4.txt
+    cat ./Cache/Geoip/Process/Source/IPv4.txt
 #    cat ./Cache/Geoip/Upstream/Hackl0us/GeoIP2-CN/CN-ip-cidr.txt
     cat ./Cache/Geoip/Process/misakaio/chnroutes2/Geoip.txt
     cat ./Cache/Geoip/Upstream/pexcn/daily/chnroute.txt
@@ -20,7 +24,7 @@ sed -e '/\./d' ./Cache/Geoip/Upstream/Loyalsoldier/geoip/private.txt > ./Cache/G
 sort -u ./Cache/Geoip/Process/IPv4-1.txt > ./Cache/Geoip/Process/IPv4.txt
 
 {
-    cat ./Source/Geoip/Apple_Push_Notification_Service/IPv6.txt
+    cat ./Cache/Geoip/Process/Source/IPv6.txt
     cat ./Cache/Geoip/Upstream/Clang.CN/all_cn_ipv6.txt
     cat ./Cache/Geoip/Upstream/pexcn/daily/chnroute-v6.txt
     cat ./Cache/Geoip/Process/Loyalsoldier/geoip/private-ipv6.txt
