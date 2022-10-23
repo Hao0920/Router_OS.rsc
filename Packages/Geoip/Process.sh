b17d3f42c9b584de68a1058fed92097b8d16a9ee
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-10-23 20:07:52 +0800

    Update Process.sh

--- Packages/Geoip/Process.sh
+++ Packages/Geoip/Process.sh
@@ -14,6 +14,9 @@ sed -e '/\./d' ./Cache/Geoip/Upstream/Loyalsoldier/geoip/private.txt > ./Cache/G
 mkdir -p ./Cache/Geoip/Process/Source/
 find ./Source/Geoip/IPv4 -type f -name "*.txt" | xargs cat > ./Cache/Geoip/Process/Source/IPv4.txt
 find ./Source/Geoip/IPv6 -type f -name "*.txt" | xargs cat > ./Cache/Geoip/Process/Source/IPv6.txt
+
+# 整合Geoip
+## IPv4
 {
     cat ./Cache/Geoip/Process/Source/IPv4.txt
 #    cat ./Cache/Geoip/Upstream/Hackl0us/GeoIP2-CN/CN-ip-cidr.txt
@@ -24,6 +27,7 @@ find ./Source/Geoip/IPv6 -type f -name "*.txt" | xargs cat > ./Cache/Geoip/Proce
 } > ./Cache/Geoip/Process/IPv4-1.txt
 sort -u ./Cache/Geoip/Process/IPv4-1.txt > ./Cache/Geoip/Process/IPv4.txt
 
+## IPv6
 {
     cat ./Cache/Geoip/Process/Source/IPv6.txt
     cat ./Cache/Geoip/Upstream/Clang.CN/all_cn_ipv6.txt
@@ -32,6 +36,7 @@ sort -u ./Cache/Geoip/Process/IPv4-1.txt > ./Cache/Geoip/Process/IPv4.txt
 } > ./Cache/Geoip/Process/IPv6-1.txt
 sort -u ./Cache/Geoip/Process/IPv6-1.txt > ./Cache/Geoip/Process/IPv6.txt
 
+# Forward DNS
 ## Loyalsoldier/v2ray-rules-dat
 mkdir -p ./Cache/Geoip/Process/Loyalsoldier/v2ray-rules-dat
 sed -e 's/\./\\\\\./g' ./Cache/Geoip/Upstream/Loyalsoldier/v2ray-rules-dat/gfw.txt > ./Cache/Geoip/Process/Loyalsoldier/v2ray-rules-dat/gfw-1.txt
