84fbcdbe18d78098c9093d1d6942685d0e362a3f
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-12-02 19:53:18 +0800

    Update Process.sh

--- Packages/Geoip/Process.sh
+++ Packages/Geoip/Process.sh
@@ -18,7 +18,7 @@ find ./Source/Geoip/IPv6 -type f -name "*.txt" | xargs cat > ./Cache/Geoip/Proce
 # 整合Geoip
 ## IPv4
 {
-    cat ./Cache/Geoip/Process/Source/IPv4.txt
+#    cat ./Cache/Geoip/Process/Source/IPv4.txt
 #    cat ./Cache/Geoip/Upstream/Hackl0us/GeoIP2-CN/CN-ip-cidr.txt
     cat ./Cache/Geoip/Process/misakaio/chnroutes2/Geoip.txt
     cat ./Cache/Geoip/Upstream/pexcn/daily/chnroute.txt
@@ -29,7 +29,7 @@ sort -u ./Cache/Geoip/Process/IPv4-1.txt > ./Cache/Geoip/Process/IPv4.txt
 
 ## IPv6
 {
-    cat ./Cache/Geoip/Process/Source/IPv6.txt
+#    cat ./Cache/Geoip/Process/Source/IPv6.txt
     cat ./Cache/Geoip/Upstream/Clang.CN/all_cn_ipv6.txt
     cat ./Cache/Geoip/Upstream/pexcn/daily/chnroute-v6.txt
 #    cat ./Cache/Geoip/Process/Loyalsoldier/geoip/private-ipv6.txt
