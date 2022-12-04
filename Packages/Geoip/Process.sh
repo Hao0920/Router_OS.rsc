e7df29626e0b92fe66dbb2fc9fb7112d74f2af2f
Hao0920 <98888427+Hao0920@users.noreply.github.com>
2022-12-04 16:25:47 +0800

    Update Process.sh

--- Packages/Geoip/Process.sh
+++ Packages/Geoip/Process.sh
@@ -18,21 +18,18 @@ find ./Source/Geoip/IPv6 -type f -name "*.txt" | xargs cat > ./Cache/Geoip/Proce
 # 整合Geoip
 ## IPv4
 {
-#    cat ./Cache/Geoip/Process/Source/IPv4.txt
-    cat ./Cache/Geoip/Upstream/Hackl0us/GeoIP2-CN/CN-ip-cidr.txt
+    cat ./Cache/Geoip/Process/Source/IPv4.txt
     cat ./Cache/Geoip/Process/misakaio/chnroutes2/Geoip.txt
     cat ./Cache/Geoip/Upstream/pexcn/daily/chnroute.txt
     cat ./Cache/Geoip/Upstream/Clang.CN/all_cn_cidr.txt
-#    cat ./Cache/Geoip/Process/Loyalsoldier/geoip/private-ipv4.txt
 } > ./Cache/Geoip/Process/IPv4-1.txt
 sort -u ./Cache/Geoip/Process/IPv4-1.txt > ./Cache/Geoip/Process/IPv4.txt
 
 ## IPv6
 {
-#    cat ./Cache/Geoip/Process/Source/IPv6.txt
+    cat ./Cache/Geoip/Process/Source/IPv6.txt
     cat ./Cache/Geoip/Upstream/Clang.CN/all_cn_ipv6.txt
     cat ./Cache/Geoip/Upstream/pexcn/daily/chnroute-v6.txt
-#    cat ./Cache/Geoip/Process/Loyalsoldier/geoip/private-ipv6.txt
 } > ./Cache/Geoip/Process/IPv6-1.txt
 sort -u ./Cache/Geoip/Process/IPv6-1.txt > ./Cache/Geoip/Process/IPv6.txt
 
